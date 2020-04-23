ea5e0aed4df8a596228829ac1b10f521827c158e
carden <carden.ruby@gmail.com>
2020-04-23 22:07:42 +0300

    modification

--- game.rb
+++ game.rb
@@ -1,25 +1,24 @@
 #
 class Game
-  @cards = %w[A+ A<3 A^ A<> K+ K<3 K^ K<> D+ D<3 D^ D<> J+ J<3 J^ J<>
-              10+ 10<3 10^ 10<> 9+ 9<3 9^ 9<> 8+ 8<3 8^ 8<> 7+ 7<3 7^ 7<>
-              6+ 6<3 6^ 6<> 5+ 5<3 5^ 5<> 4+ 4<3 4^ 4<> 3+ 3<3 3^ 3<>
-              2+ 2<3 2^ 2<>]
-  attr_reader :name, :users
+  attr_reader :name, :user, :cards, :dealer
 
   def initialize
     @name = 'Black_Jack'
-    @users = []
+    @user = []
+    @dealer = []
+    @cards = %w[A♣ A♥ A♠ A♦ K♣ K♥ K♠ K♦ D♣ D♥ D♠ D♦ J♣ J♥ J♠ J♦ 10♣ 10♥ 10♠ 10♦
+                9♣ 9♥ 9♠ 9♦ 8♣ 8♥ 8♠ 8♦ 7♣ 7♥ 7♠ 7♦ 6♣ 6♥ 6♠ 6♦ 5♣ 5♥ 5♠ 5♦
+                4♣ 4♥ 4♠ 4♦ 3♣ 3♥ 3♠ 3♦ 2♣ 2♥ 2♠ 2♦].sort_by! { rand }
   end
 
   # Метод add_user добавляет пользователя.
   def add_user(user)
-    @users << user
+    @user << user
+    @dealer << Dealer.new
   end
 
   def deal_cards
-    
-
+    @user[0].cards << @cards.pop(2)
+    @dealer[0].cards << @cards.pop(2)
   end
-
-
 end
