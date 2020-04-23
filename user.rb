e55fa295676d473f54286b9ce08858039a8b7a18
carden <carden.ruby@gmail.com>
2020-04-23 22:54:57 +0300

    cards sum

--- user.rb
+++ user.rb
@@ -1,9 +1,14 @@
 #
 class User
-  attr_reader :name, :money, :cards
+  attr_reader :name, :money, :cards, :sum_cards
   def initialize(name)
     @name = name
     @money = 100
     @cards = []
+    @sum_cards = 0
+  end
+
+  def cards_sum
+    @sum_cards = @cards[0][0][1] + @cards[0][1][1]
   end
 end
