dbf3839094e218c51b36be7bc5fd222bb757d8da
carden <carden.ruby@gmail.com>
2020-04-24 15:42:43 +0300

    modification

--- dealer.rb
+++ dealer.rb
@@ -1,9 +1,15 @@
 #
 class Dealer
-  attr_reader :money, :name, :cards
+  attr_accessor :money, :sum_cards
+  attr_reader :name, :cards
   def initialize
     @name = 'Dealer'
     @money = 100
     @cards = []
+    @sum_cards = 0
+  end
+
+  def cards_sum
+    @sum_cards = @cards[0][0][1] + @cards[0][1][1]
   end
 end
