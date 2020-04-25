b2cfcc782692805e5ab4edede7b96871e4ffe855
carden <carden.ruby@gmail.com>
2020-04-25 13:49:20 +0300

    comments

--- user.rb
+++ user.rb
@@ -1,7 +1,11 @@
-#
+# Класс User содержит информацию о пользователе:
+# Содержит имя
+# Колличество денег
+# Карты и сумму очков
 class User
   attr_accessor :money, :sum_cards
   attr_reader :name, :cards
+
   def initialize(name)
     @name = name
     @money = 100
@@ -9,6 +13,7 @@ class User
     @sum_cards = 0
   end
 
+  # Метод cards_sum считает сумму очков первых двух карт.
   def cards_sum
     @sum_cards = @cards[0][0][1] + @cards[0][1][1]
   end
