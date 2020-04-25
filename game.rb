2a46e8097fa3bbb5a420dbec891dd74ea3fafa4c
carden <carden.ruby@gmail.com>
2020-04-25 13:38:00 +0300

    modification

--- game.rb
+++ game.rb
@@ -1,5 +1,11 @@
-#
+# Класс Game содержит механику игры.
+# Может принимать пользователя и диллера
+# Может перемешивать и раздавать карты игрокам
+# Может подсчитывать суму очков
+# Может принимать ставку в Банк
+# Может выявлять победителя в игре.
 class Game
+  # HASH содержит методы.
   HASH = { '1' => :new_game, '2' => :add_card_dealer,
            '3' => :add_card_user, '4' => :open_cards }.freeze
 
@@ -60,6 +66,7 @@ class Game
   # и считает суммы очков.
   def new_game
     return unless @bank.zero?
+
     if @user.size.zero?
       message = ['Введите ваше имя:']
       name = data_input(message).first
@@ -69,6 +76,7 @@ class Game
       @user[0].cards.clear && @user[0].sum_cards = 0
       @dealer[0].cards.clear && @dealer[0].sum_cards = 0
     end
+
     if @user[0].money.positive? && @dealer[0].money.positive?
       deal_cards
       make_a_bet
@@ -91,10 +99,13 @@ class Game
                '5♠' => 5, '5♦' => 5, '4♣' => 4, '4♥' => 4, '4♠' => 4, '4♦' => 4,
                '3♣' => 3, '3♥' => 3, '3♠' => 3, '3♦' => 3, '2♣' => 2, '2♥' => 2,
                '2♠' => 2, '2♦' => 2 }.sort.sort_by! { rand }
+
     @user[0].cards << @cards.pop(2)
     @dealer[0].cards << @cards.pop(2)
+
     @user[0].cards_sum
     @user[0].sum_cards = 12 if @user[0].cards_sum == 22
+
     @dealer[0].cards_sum
     @dealer[0].sum_cards = 12 if @dealer[0].cards_sum == 22
   end
@@ -104,6 +115,7 @@ class Game
   def make_a_bet
     @user[0].money -= @bet
     @bank += @bet
+
     @dealer[0].money -= @bet
     @bank += @bet
   end
@@ -111,34 +123,43 @@ class Game
   # Метод add_card_user добавляет дополнительную карту пользователю.
   def add_card_user
     return if @bank.zero?
+
     return if @user[0].cards[0].size == 3
 
     @user[0].cards[0] << @cards.pop if @user[0].cards[0].size < 3
+
     if @user[0].cards[0].last.last == 11 && @user[0].sum_cards + 11 > 21
       @user[0].sum_cards += 1
     elsif @user[0].cards[0].size > 2
       @user[0].sum_cards += @user[0].cards[0].last.last
     end
+
     open_cards if @user[0].sum_cards > 21
   end
 
   # Метод add_card_dealer добавляет дополнительную карту диллеру.
   def add_card_dealer
     return if @bank.zero?
+
     return if @dealer[0].cards[0].size == 3
+
     @dealer[0].cards[0] << @cards.pop if @dealer[0].sum_cards < 17 &&
                                          @user[0].sum_cards > @dealer[0].sum_cards &&
                                          @user[0].sum_cards <= 21
+
     if @dealer[0].cards[0].last.last == 11 && @dealer[0].sum_cards + 11 > 21
       @dealer[0].sum_cards += 1
     elsif @dealer[0].cards[0].size > 2
       @dealer[0].sum_cards += @dealer[0].cards[0].last.last
     end
+
     open_cards if @dealer[0].cards[0].size == 3 || @dealer[0].sum_cards >= 17
   end
 
+  # Метод open_cards производит подсчёт очков и выявляет победителя.
   def open_cards
     add_card_dealer if @dealer[0].cards.size < 3 && @dealer[0].sum_cards < 17
+
     if @user[0].sum_cards > @dealer[0].sum_cards && @user[0].sum_cards <= 21
       @user[0].money += @bank
       @bank = 0
@@ -168,22 +189,26 @@ class Game
     end
   end
 
+  # Метод message_new_round выводит сообщение.
   def message_new_round
     puts "Если хотите продолжить нажмите: '1', если нет нажмите: '0'\n\n"
   end
 
+  # Метод message_dealer_win выводит сообщение.
   def message_dealer_win
     puts "Диллер победил.\n\n"
     puts "Сумма очков Диллера: #{@dealer[0].sum_cards}\n\n"
     puts "Сумма очков #{@user[0].name}: #{@user[0].sum_cards}\n\n"
   end
 
+  # Метод message_user_win выводит сообщение.
   def message_user_win
     puts "Игрок #{@user[0].name} - Победил!\n\n"
     puts "Сумма очков #{@user[0].name}: #{@user[0].sum_cards}\n\n"
     puts "Сумма очков Диллера: #{@dealer[0].sum_cards}\n\n"
   end
 
+  # Метод message_draw выводит сообщение.
   def message_draw
     puts "Ничья!\n\n"
     puts "Сумма очков #{@user[0].name}: #{@user[0].sum_cards}\n\n"
