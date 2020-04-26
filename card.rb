a1c8f68ac5c5074b38707bb9fc57f96c6feb6892
carden <carden.ruby@gmail.com>
2020-04-26 20:50:25 +0300

    modification

--- card.rb
+++ card.rb
@@ -1,7 +1,20 @@
 #
 class Card
-  def initialize(suit, value)
+  attr_reader :suit, :rank, :value
+
+  def initialize(suit, rank)
     @suit = suit
-    @value = value
+    @rank = rank
+    @value = find_value(rank)
+  end
+
+  private
+
+  def find_value(rank)
+    return 11 if rank == 'A'
+    return 10 if rank == 'J'
+    return 10 if rank == 'Q'
+    return 10 if rank == 'K'
+    return rank if rank.class == Integer
   end
 end
