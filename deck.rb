de1aeb4b58be1a5b29d5a6b2e81eb5537083d1ed
carden <carden.ruby@gmail.com>
2020-04-26 16:23:51 +0300

    rand

--- deck.rb
+++ deck.rb
@@ -3,10 +3,11 @@ class Deck
   SUITS = %w[♠ ♥ ♣ ♦].freeze
   VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].freeze
 
-  attr_reader :cards
+  attr_reader :cards, :cards_rand
 
   def initialize
     @cards = []
     SUITS.map { |suit| VALUES.map { |value| @cards << Card.new(suit, value) } }
+    @cards_rand = @cards.sort_by { rand }
   end
 end
