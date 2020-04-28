457ee1c506ff25829735efdec4d8bdd1c9101bf7
carden <carden.ruby@gmail.com>
2020-04-28 23:25:27 +0300

    modification

--- terminal_interface.rb
+++ terminal_interface.rb
@@ -1,8 +1,9 @@
 # Интерфейс терминала.
 class TerminalInterface
   # HASH содержит методы.
-  HASH = { '1' => :new_round, '2' => :add_card_dealer,
-           '3' => :add_card_user, '4' => :pay_to_winner }.freeze
+  HASH = { '1' => :add_card_dealer, '2' => :add_card_user,
+           '3' => :pay_to_winner }.freeze
+
   BORDERLINE = '-' * 50
   NEWLINE = "\n" * 2
 
@@ -10,33 +11,32 @@ class TerminalInterface
 
   def initialize(game)
     @game = game
+    start
     start_game
   end
 
+  def start
+    puts 'Введите Ваше имя:'
+    name = gets.chomp
+    game.user.name = name
+  end
+
   def start_game
-    # Цикл с игрой - опрос состояния и показ возможных действий
+    game.new_round
     puts NEWLINE
-    puts "Добро пожаловать в игру 'Black_Jack'"
+    puts "#{game.user.name} Добро пожаловать в игру 'Black_Jack'"
     puts NEWLINE
 
     loop do
       puts "bank: #{game.bank}\n\n"
       puts NEWLINE
-      menu_items
 
-      menu_item = gets.chomp
-
-      break unless menu_item != '0'
-
-      # Запуск меню (Цикл). С запросом ввода нужного пользователю пункта
-      # и передаёт результат в виде параметра методу selected.
-      selected(menu_item)
       puts BORDERLINE
       puts "Ваши деньги: #{game.user.money}\n\n"
       puts 'Ваши карты:'
       game.user.cards.flatten.each { |elem| print "#{elem.rank}#{elem.suit}  " }
       puts NEWLINE
-      puts "Сумма очков: #{game.user.sum_cards}\n\n"
+      puts "Сумма очков: #{game.user.sum_cards}"
       puts BORDERLINE
 
       if game.bank != 0
@@ -46,48 +46,93 @@ class TerminalInterface
         puts 'Карты Диллера:'
         game.dealer.cards.flatten.each { |elem| print "#{elem.rank}#{elem.suit}  " }
         puts NEWLINE
-        puts "Сумма очков: #{game.dealer.sum_cards}\n\n"
+        puts "Сумма очков: #{game.dealer.sum_cards}"
       end
       puts BORDERLINE
+
+      if game.bank != 0
+        start_menu
+        start_menu = gets.chomp
+        selected(start_menu)
+      elsif game.user.money.zero? || game.dealer.money.zero?
+        menu_3
+        menu = gets.chomp
+        selected_3(menu)
+      elsif game.cards.cards.size < 52
+        menu_2
+        menu = gets.chomp
+        selected_2(menu)
+      end
+      break unless menu != '0'
     end
   end
 
-  # Метод menu_items выводит пользовательское меню.
-  def menu_items
+
+  # Метод selected принимает параметр из пользовательского ввода
+  # и исполняет соответствующий метод.
+  def selected(start_menu)
+    game.send HASH[start_menu]
+
+    game.send HASH['3'] if game.user.sum_cards > 21
+
+    game.send HASH['3'] if game.dealer.cards.flatten.size == 3 ||
+                           game.dealer.sum_cards >= 17 ||
+                           game.dealer.sum_cards >= 21
+
+    game.send HASH['1'] if start_menu == '3' &&
+                           game.dealer.cards.flatten.size < 3 &&
+                           game.dealer.sum_cards < 17
+    messages
+  rescue TypeError
+    message_re_enter
+  end
+
+  def messages
+    message_user_win if game.bank.zero?
+    message_new_round if game.bank.zero?
+    message_no_money if game.user.money.zero?
+    message_win if game.dealer.money.zero?
+  end
+
+  def selected_2(menu)
+    game.new_round if menu == '1'
+  end
+
+  def start_menu
     messages = ['Выберите действие, введя номер из списка: ',
                 BORDERLINE,
-                ' 1 - Начать игру.',
-                ' 2 - Пропустить.',
-                ' 3 - Добавить карту.',
-                ' 4 - Открыть карты.',
+                ' 1 - Пропустить.',
+                ' 2 - Добавить карту.',
+                ' 3 - Открыть карты.',
+                BORDERLINE]
+    messages.each { |item| puts item }
+  end
+
+  def menu_2
+    messages = ['Выберите действие, введя номер из списка: ',
                 BORDERLINE,
-                '  0 - Для выхода из программы.']
+                ' 1 - Продолжить.',
+                ' 0 - Выйти из игры.',
+                BORDERLINE]
+    messages.each { |item| puts item }
+  end
+
+  def selected_3(menu)
+    BlackJack.new if menu == '1'
+  end
+
+  def menu_3
+    messages = ['Выберите действие, введя номер из списка: ',
+                BORDERLINE,
+                ' 1 - Новая игра.',
+                ' 0 - Выйти из игры.',
+                BORDERLINE]
     messages.each { |item| puts item }
   end
 
   # Метод message_re_enter выводит сообщение.
   def message_re_enter
     puts "Повторите ввод.\n\n"
-  end
-
-  # Метод selected принимает параметр из пользовательского ввода
-  # и исполняет соответствующий метод.
-  def selected(menu_item)
-    game.send HASH[menu_item]
-    message_user_win if game.bank.zero?
-    message_new_round if game.bank.zero?
-    message_no_money if game.user.money.zero?
-    message_win if game.dealer.money.zero?
-  rescue TypeError
-    message_re_enter
-  end
-
-  # Метод data_input принимает параметр печатает его
-  # и запрашивает ввод пользователя, результат сохранят в @args.
-  def data_input(message)
-    @args = []
-    message.each { |mess| puts mess }
-    @args << gets.chomp
   end
 
   # Метод message_no_money выводит сообщение.
