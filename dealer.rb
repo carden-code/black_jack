081435d733395d276bfbc448c56ba4e95a59ea7a
carden <carden.ruby@gmail.com>
2020-04-29 21:44:44 +0300

    modification

--- dealer.rb
+++ dealer.rb
@@ -3,4 +3,12 @@ class Dealer < User
   def initialize
     super 'Dealer'
   end
+
+  # Метод add_card_dealer добавляет дополнительную карту диллеру.
+  def take_card(deck)
+    return if @cards.size == 3
+
+    @cards << deck.deal_cards if @sum_cards < 17
+    cards_sum
+  end
 end
