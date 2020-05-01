e9b92a11d741354381ebdfc0f9cc9de66588dc98
carden <carden.ruby@gmail.com>
2020-05-01 19:51:18 +0300

    modification

--- dealer.rb
+++ dealer.rb
@@ -6,7 +6,7 @@ class Dealer < User
 
   # Метод add_card_dealer добавляет дополнительную карту диллеру.
   def take_card(deck)
-    return if @cards.size == 3
+    #return if @cards.size == 3
 
     @cards << deck.deal_cards if @sum_cards < 17
     cards_sum
